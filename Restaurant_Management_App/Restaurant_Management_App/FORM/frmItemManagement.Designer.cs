7952622320fb18d15bd37b25165ee6d4197e25cd
DuyKhoi282 <tranquocduykhoi@gmail.com>
2026-04-01 14:22:56 +0700

    test chức năng của hàm Load

--- Restaurant_Management_App/Restaurant_Management_App/FORM/frmItemManagement.Designer.cs
+++ Restaurant_Management_App/Restaurant_Management_App/FORM/frmItemManagement.Designer.cs
@@ -28,33 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // lblTest
             // 
-            this.button1.Location = new System.Drawing.Point(264, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 97);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.Location = new System.Drawing.Point(229, 91);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(283, 91);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "DEMO";
             // 
             // frmItemManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTest);
             this.Name = "frmItemManagement";
             this.Text = "frmItemManagement";
             this.Load += new System.EventHandler(this.frmItemManagement_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTest;
     }
 }
