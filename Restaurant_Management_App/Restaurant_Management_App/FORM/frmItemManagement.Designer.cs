8757c4eabecfc4a1ef310271b6835937029fcd54
DuyKhoi282 <tranquocduykhoi@gmail.com>
2026-04-01 17:05:16 +0700

    code sơ bộ form ItemManagement
    Đưa sơ bộ thông tin món ăn + thanh tìm kiếm

--- Restaurant_Management_App/Restaurant_Management_App/FORM/frmItemManagement.Designer.cs
+++ Restaurant_Management_App/Restaurant_Management_App/FORM/frmItemManagement.Designer.cs
@@ -28,28 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmItemManagement));
             this.lblTest = new System.Windows.Forms.Label();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddCate = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlSearch = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.pnlData = new System.Windows.Forms.Panel();
+            this.dgvFood = new System.Windows.Forms.DataGridView();
+            this.pnlTop.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFood)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTest
             // 
             this.lblTest.AutoSize = true;
-            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTest.Location = new System.Drawing.Point(229, 91);
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.Location = new System.Drawing.Point(12, 9);
             this.lblTest.Name = "lblTest";
-            this.lblTest.Size = new System.Drawing.Size(283, 91);
+            this.lblTest.Size = new System.Drawing.Size(103, 20);
             this.lblTest.TabIndex = 0;
-            this.lblTest.Text = "DEMO";
+            this.lblTest.Text = "Category list";
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.LightGray;
+            this.pnlTop.Controls.Add(this.pnlSearch);
+            this.pnlTop.Controls.Add(this.label1);
+            this.pnlTop.Controls.Add(this.btnAddCate);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Padding = new System.Windows.Forms.Padding(10);
+            this.pnlTop.Size = new System.Drawing.Size(1052, 79);
+            this.pnlTop.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Category List";
+            // 
+            // btnAddCate
+            // 
+            this.btnAddCate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAddCate.BackColor = System.Drawing.Color.IndianRed;
+            this.btnAddCate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAddCate.BackgroundImage")));
+            this.btnAddCate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAddCate.FlatAppearance.BorderSize = 0;
+            this.btnAddCate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddCate.Location = new System.Drawing.Point(3, 22);
+            this.btnAddCate.Name = "btnAddCate";
+            this.btnAddCate.Size = new System.Drawing.Size(40, 40);
+            this.btnAddCate.TabIndex = 3;
+            this.btnAddCate.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 629);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1052, 0);
+            this.panel3.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1052, 0);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.BackColor = System.Drawing.Color.White;
+            this.pnlSearch.Controls.Add(this.txtSearch);
+            this.pnlSearch.Controls.Add(this.pictureBox1);
+            this.pnlSearch.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlSearch.Location = new System.Drawing.Point(561, 10);
+            this.pnlSearch.Name = "pnlSearch";
+            this.pnlSearch.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlSearch.Size = new System.Drawing.Size(481, 59);
+            this.pnlSearch.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(48, 5);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(428, 49);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // pnlData
+            // 
+            this.pnlData.Controls.Add(this.dgvFood);
+            this.pnlData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlData.Location = new System.Drawing.Point(0, 79);
+            this.pnlData.Name = "pnlData";
+            this.pnlData.Size = new System.Drawing.Size(1052, 550);
+            this.pnlData.TabIndex = 5;
+            // 
+            // dgvFood
+            // 
+            this.dgvFood.AllowUserToAddRows = false;
+            this.dgvFood.AllowUserToDeleteRows = false;
+            this.dgvFood.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFood.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFood.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFood.EnableHeadersVisualStyles = false;
+            this.dgvFood.Location = new System.Drawing.Point(0, 0);
+            this.dgvFood.MultiSelect = false;
+            this.dgvFood.Name = "dgvFood";
+            this.dgvFood.ReadOnly = true;
+            this.dgvFood.RowHeadersVisible = false;
+            this.dgvFood.RowHeadersWidth = 51;
+            this.dgvFood.RowTemplate.Height = 24;
+            this.dgvFood.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFood.Size = new System.Drawing.Size(1052, 550);
+            this.dgvFood.TabIndex = 0;
             // 
             // frmItemManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1052, 629);
+            this.Controls.Add(this.pnlData);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pnlTop);
             this.Controls.Add(this.lblTest);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmItemManagement";
             this.Text = "frmItemManagement";
             this.Load += new System.EventHandler(this.frmItemManagement_Load);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlData.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFood)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +214,15 @@
         #endregion
 
         private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddCate;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel pnlSearch;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Panel pnlData;
+        private System.Windows.Forms.DataGridView dgvFood;
     }
 }
--- Restaurant_Management_App/Restaurant_Management_App/FORM/frmItemManagement.cs
+++ Restaurant_Management_App/Restaurant_Management_App/FORM/frmItemManagement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,105 @@ namespace Restaurant_Management_App.FORM
 
         private void frmItemManagement_Load(object sender, EventArgs e)
         {
-           
+            SetupDataGridView();
+            loadFoodItems();
+            addButtonColumns();
+            FormatGridColumns();
+        }
+        private void SetupDataGridView()//Hàm này dùng để thiết lập giao diện cho DataGridView, bao gồm màu nền, màu chữ, kiểu đường viền, v.v.
+        {
+
+            dgvFood.Dock = DockStyle.Fill;
+            dgvFood.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvFood.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvFood.MultiSelect = false;
+            dgvFood.ReadOnly = true;
+            dgvFood.AllowUserToAddRows = false;
+            dgvFood.RowHeadersVisible = false;
+            // Header style
+            dgvFood.EnableHeadersVisualStyles = false;
+            dgvFood.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(44, 62, 80);
+            dgvFood.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvFood.ColumnHeadersHeight = 40;
+            // Row style
+            dgvFood.RowTemplate.Height = 35;
+            dgvFood.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 245, 245);
+            dgvFood.DefaultCellStyle.SelectionBackColor = Color.FromArgb(52, 152, 219);
+            dgvFood.DefaultCellStyle.SelectionForeColor = Color.White;
+            // Border
+            dgvFood.BorderStyle = BorderStyle.None;
+            dgvFood.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgvFood.GridColor = Color.LightGray;
+        }
+        private void addButtonColumns()//Hàm này dùng để thêm cột chứa nút Edit và Delete vào DataGridView
+        {
+            if (dgvFood.Columns["colEdit"] == null)
+            {
+                if (dgvFood.Columns["colEdit"] == null)//Kiểm tra nếu cột Edit chưa tồn tại thì mới thêm vào để tránh việc thêm nhiều cột Edit khi load lại dữ liệu
+                {
+                    DataGridViewButtonColumn btnEdit = new DataGridViewButtonColumn();
+                    btnEdit.Name = "colEdit";
+                    btnEdit.HeaderText = "";
+                    btnEdit.Text = "Edit";
+                    btnEdit.UseColumnTextForButtonValue = true;
+                    dgvFood.Columns.Add(btnEdit);
+                }
+
+                if (dgvFood.Columns["colDelete"] == null)//Kiểm tra nếu cột Delete chưa tồn tại thì mới thêm vào để tránh việc thêm nhiều cột Delete khi load lại dữ liệu
+                {
+                    DataGridViewButtonColumn btnDelete = new DataGridViewButtonColumn();
+                    btnDelete.Name = "colDelete";
+                    btnDelete.HeaderText = "";
+                    btnDelete.Text = "Delete";
+                    btnDelete.UseColumnTextForButtonValue = true;
+                    dgvFood.Columns.Add(btnDelete);
+                }
+            }
+        }
+        private void loadFoodItems()//Hàm này dùng để load dữ liệu món ăn vào DataGridView, hiện tại đang là dữ liệu giả để test giao diện
+        {
+            string query = "SELECT id, name, price FROM Food";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, Database.connStr);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dgvFood.DataSource = table;
+        }
+
+        private void FormatGridColumns()//Định dạng lại kích thước của các cột trong DataGridView, bao gồm cột id, cột Edit và cột Delete 
+        {
+            dgvFood.Columns["id"].Width = 50;
+
+            dgvFood.Columns["colEdit"].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            dgvFood.Columns["colDelete"].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+
+            dgvFood.Columns["colEdit"].Width = 80;
+            dgvFood.Columns["colDelete"].Width = 80;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)//Tìm kiếm món ăn theo tên khi người dùng nhập vào ô tìm kiếm
+        {
+            string keyword = txtSearch.Text.Trim();
+
+            if (keyword == "")//Nếu ô tìm kiếm trống thì load lại tất cả món ăn
+            {
+                loadFoodItems();
+                return;
+            }
+
+            string query = @"SELECT id, name, price FROM Food WHERE name COLLATE Latin1_General_CI_AI LIKE @key";//Hàm giúp không phân biệt chữ hoa chữ thường và dấu khi tìm kiếm món ăn theo tên
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, Database.connStr);
+            adapter.SelectCommand.Parameters.AddWithValue("@key", "%" + keyword + "%");//Thêm tham số tìm kiếm vào câu truy vấn để tránh lỗi SQL Injection
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);//Đổ dữ liệu tìm kiếm vào DataTable
+
+            dgvFood.DataSource = table;//Hiển thị kết quả tìm kiếm lên DataGridView
+
+            addButtonColumns();
         }
     }
 }
+
